61901b39b7c8a66a8fcbc0b7e4f5a44bccd8f5a4
Bartechaosp <69191597+Bartechaosp@users.noreply.github.com>
2023-04-26 14:56:23 +0200

    Update elo.php

--- elo.php
+++ elo.php
@@ -79,5 +79,93 @@
     //     return strlen($napis);
     // }
     // echo ciagZnakow("Elo");
-        //zad 15
+
+    //zad 9
+    // $liczba1 = $_POST['input1'];
+    // $liczba2 = $_POST['input2'];
+
+    // function iloczyn($liczba1, $liczba2) {
+    //     return $liczba1 * $liczba2;
+    // }
+    // echo iloczyn($liczba1, $liczba2);
+
+    //zad 10
+    // function reverse($napis) {
+    //     for ($i = strlen($napis)-1; $i >= 0 ;$i--) {
+    //         echo $napis[$i];
+    //     }
+    // }
+    // reverse("elo");
+
+    //zad 11
+    // function ktoWiekszy($a, $b) {
+    //     if ($a > $b) {
+    //         return $a;
+    //     } else return $b;
+    // }
+    // echo ktoWiekszy(2, 4);
+
+    //zad 12
+    // $tekst = $_POST['input1'];
+    // function reverse($napis) {
+    //     for ($i = strlen($napis)-1; $i >= 0 ;$i--) {
+    //         echo $napis[$i];
+    //     }
+    // }
+    // reverse($tekst);
+
+    //zad 13
+    //     function sredniaZTablicy($tablica) {
+    //         $suma = 0;
+    //         for ($i = 0; $i < count($tablica) ;$i++) {
+    //             $suma += $tablica[$i];
+    //         }
+    //         return $suma / count($tablica);
+    //     }
+
+    // $tab = [1,2,3];
+
+    // echo sredniaZTablicy($tab);
+
+    //zad 14
+    // for ($i = 1; $i < 10 ;$i++) {
+    //     for ($y = 1; $y < 10 ;$y++) {
+    //         $z = $i * $y;
+    //         echo "$i * $y = $z\r\n";
+    //     }
+    // }
+
+    //zad 15
+    // while(True) {
+    //     echo "1 - wylicz śilnię z podanej liczby\n";
+    //     echo "2 - Spotęguj podstawę potęgi do wykładnika potęgi\n";
+    //     echo "3 - niespodzianka\n";
+    //     echo "4 - zamknij program\n";
+
+    //     $optionMain = readline("Podaj opcję: ");
+    //     if ($optionMain == 1) {
+    //         $liczba = readline("Podaj liczbę do policzenia silni: ");
+    //         $silnia = 1;
+    //         for ($i = $liczba-1; $i > 0 ;$i--) {
+    //             $silnia *= $i;
+    //         }
+    //         echo "$silnia\n";
+    //     } else if ($optionMain == 2) {
+    //         $podstawa = readline("Podaj podstawę potęgi: ");
+    //         $wykladnik = readline("Podaj wykładnik potęgi: ");
+    //         $wynik = pow($podstawa, $wykladnik);
+    //         echo "$wynik\n";
+    //     } else if ($optionMain == 3) {
+    //         $tekst = "PHP jest cool :-f";
+    //         for ($i = 0; $i < strlen($tekst) ;$i++) {
+    //             echo "$tekst[$i] ";
+    //         }
+    //         echo "\n";
+    //     } else if ($optionMain == 4) {
+    //         break;
+    //     }
+    // }
+
+    //zad 16
+    //echo pi();
 ?> 
