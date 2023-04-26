5b5c35e868bee8754fd911650990d057d9428893
Bartechaosp <69191597+Bartechaosp@users.noreply.github.com>
2023-04-26 14:06:43 +0200

    Update elo.php

--- elo.php
+++ elo.php
@@ -14,7 +14,7 @@
         // echo $suma;
 
         //zad 3
-        // $number = $_POST['input'];
+        // $number = $_POST['input1'];
         // if ($number % 2 == 0) {
         //     echo("Liczba jest parzysta");
         // } else {
@@ -22,12 +22,62 @@
         // }
 
         //zad 4
-        $text = $_POST['input'];
-        if($text == strrev($text)) {
-            echo "to słowo jest tym czymś";
-        } else {
-            echo "Nie jest tym czymś";
-        }
+        // $text = $_POST['input1'];
+        // for ($i = 0; $i < strlen($text) ;$i++) {
+        //     if($text[$i] == " " || $text[$i] == "-" && $text[$i] == ",") {
+        //         echo "No Panie, to nie jest słowo i ";
+        //     }
+        // }
+        // if($text == strrev($text)) {
+        //     echo "to słowo jest tym czymś";
+        // } else {
+        //     echo "nie jest tym czymś";
+        // }
 
+        //zad 5
+        // function czyPierwsza($liczba) {
+        //     $count = 0;
+        //     for ($i = 1; $i <= $liczba ;$i++) {
+        //         if ($liczba % $i == 0) {
+        //             $count++;
+        //         }
+        //     }
+        //     if ($count == 2) echo $liczba;
+        // }
+        
+        // for ($i = 1; $i < 100 ;$i++) {
+        //     czyPierwsza($i);
+        //     echo "\n";
+        // }
+
+        //zad 6
+    //     function wieloZnak($napis, $powtoznik) {
+    //         str_split($napis);
+    //         for ($z=0; $z < strlen($napis);$z++) {
+    //         for ($i=0; $i < $powtoznik ;$i++) {
+    //             echo $napis[$z];
+    //         }
+    //     }
+    // }
+    //     $text = $_POST["input1"];
+    //     $powtoznik = $_POST['input2'];
+    //     wieloZnak($text, $powtoznik);
+
+    //zad 7
+    // for ($i = 1; $i < 100 ;$i++) {
+    //     if ($i % 3 == 0 && $i % 5 == 0) {
+    //         echo "FizzBuzz\n";
+    //     } else if ($i % 5 == 0) {
+    //         echo "Buzz\n";
+    //     } else if ($i % 3 == 0) {
+    //         echo "Fizz\n";
+    //     } else echo "$i\n";
+    // }
+
+    //zad 8
+    // function ciagZnakow($napis) {
+    //     return strlen($napis);
+    // }
+    // echo ciagZnakow("Elo");
         //zad 15
 ?> 
